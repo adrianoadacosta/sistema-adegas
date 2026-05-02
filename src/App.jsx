cf46cec346db61704e001bd2c04d10bff52cab44
adrianoadacosta <adrianoadacosta@outlook.com>
2026-05-02 00:49:23 -0300

    feat: cadastro de cliente integrado ao supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react'
+import { supabase } from './supabaseClient'
+
 function App() {
+  const [nome, setNome] = useState('')
+
+  async function salvarCliente() {
+    const { error } = await supabase
+      .from('clientes')
+      .insert([{ nome }])
+
+    if (error) {
+      console.log(error)
+      alert('Erro ao salvar')
+    } else {
+      alert('Cliente salvo!')
+      setNome('')
+    }
+  }
+
   return (
     <div>
       <h1>Sistema de Adegas</h1>
+
+      <input
+        placeholder="Nome do cliente"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+
+      <button onClick={salvarCliente}>
+        Salvar
+      </button>
     </div>
   )
 }
